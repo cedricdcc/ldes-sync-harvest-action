1cde75e87df44cbe40821d5cbb5072b7a0ac13de
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-24 14:09:36 +0200

    Refactor entrypoint.sh script to exclude unnecessary files and copy last_ldes_hash

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,6 +45,3 @@ echo "listing all files in the root directory"
 ls -al
 
 rsync --recursive --progress -avzh --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
-
-# copy over ./github/last_ldes_hash to ./github/workspace/.github/last_ldes_hash
-rsync --recursive --progress -avzh ./last_ldes_hash ./github/workspace/.github/last_ldes_hash
--- src/setup.sh
+++ src/setup.sh
@@ -12,7 +12,9 @@ if [ ! -d "../ldes" ]; then
     python ttl_to_yml.py
 
     # get the current hash of the repository and write it to a file called last_ldes_hash
-    git rev-parse HEAD > .././last_ldes_hash
+    cd ../github/workspace
+    git rev-parse HEAD > ./github/last_ldes_hash
+    cd ../../src
 else
     # run the ldes_sync.py script
     python ldes_sync.py
