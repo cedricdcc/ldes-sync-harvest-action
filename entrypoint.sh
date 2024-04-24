943b817e9d9c97d1381756e63374fdb84ed2e220
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-24 09:39:57 +0200

    Update Dockerfile and action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ pwd
 
 ls -al
 
-echo "config is" $1
-
 echo "repo name is:" GITHUB_REPOSITORY
 
 # copy over ./github/workspace/* /*
@@ -14,3 +12,24 @@ cd src
 
 # executing the setup.sh script
 bash setup.sh
+
+# copy over everything in ./ back to ./github/workspace
+# overwrite everything in ./github/workspace
+# do not copy over the .git folder and the .github folder 
+# also do not copy over the following files:
+# - entrypoint.sh
+# - Dockerfile
+# - README.md
+# - LICENSE
+# - .gitignore
+# - .dockerignore
+# - .env
+# package.json
+# poetry.lock
+# pyproject.toml
+# requirements.txt
+# everything in src folder and recursively under src folder
+# package-lock.json
+# actions.yml
+
+rsync --recursive --progress -avzh --exclude='.git' --exclude='.github' --exclude='entrypoint.sh' --exclude='Dockerfile' --exclude='README.md' --exclude='LICENSE' --exclude='.gitignore' --exclude='.dockerignore' --exclude='.env' --exclude='package.json' --exclude='poetry.lock' --exclude='pyproject.toml' --exclude='requirements.txt' --exclude='src' --exclude='package-lock.json' --exclude='action.yml' ./ ./github/workspace/
