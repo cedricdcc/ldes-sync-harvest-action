14d0259345d026e23a929af3a9e7c3b5e8d4241a
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-01-10 11:46:00 +0100

    introduced test for listing branches to make decisions in the future what path to take

--- src/setup.sh
+++ src/setup.sh
@@ -2,15 +2,19 @@
 
 # check if there is a ../.././github/ldes folder in the repository, if not create it
 
-if [ ! -d "../ldes" ]; then
-    mkdir "../ldes"
-    # put a file into the folder so that it is not empty
-    touch "../ldes/.gitkeep"
-    # run the ldes_download.sh script
-    bash ldes_download.sh
-    # run the ttl to yml python script
-    python ttl_to_yml.py
-else
-    # run the ldes_sync.py script
-    python ldes_sync.py
-fi
+# List all remote branches in the repository
+echo "Listing all remote branches in the repository:"
+git branch -r
+
+#if [ ! -d "../ldes" ]; then
+#    mkdir "../ldes"
+#    # put a file into the folder so that it is not empty
+#    touch "../ldes/.gitkeep"
+#    # run the ldes_download.sh script
+#    bash ldes_download.sh
+#    # run the ttl to yml python script
+#    python ttl_to_yml.py
+#else
+#    # run the ldes_sync.py script
+#    python ldes_sync.py
+#fi
