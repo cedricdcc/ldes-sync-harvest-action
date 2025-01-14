77d4198d940da215fd0ed297670bf74aa219e2fd
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-01-14 15:29:17 +0100

    Add LDES data synchronization and branch creation in setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,22 @@ if [[ $BRANCHES == *"restricted/ldes"* ]]; then
     # this means that the restricted/ldes branch exists
     # so sync must be run 
     echo "LDES sync branch exists"
+    echo "Downloading LDES data"
+    bash ldes_download.sh
+    echo "Converting TTL to YML"
+    python -u ttl_to_yml.py
+    cd .. 
+    rsync --recursive --progress -avzhq --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
+    cd ./github/workspace
+    # commit the changes
+    git add .
+    git commit -m "Syncing with LDES data"
+    git push origin main
+    git checkout restricted/ldes
+    git add .
+    git commit -m "Syncing with LDES data"
+    git push origin restricted/ldes
+    
 else
     # no restricted/ldes branch exists
     # so download must be run
@@ -15,4 +31,18 @@ else
     bash ldes_download.sh
     echo "Converting TTL to YML"
     python -u ttl_to_yml.py
+    cd .. 
+    rsync --recursive --progress -avzhq --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
+    cd ./github/workspace
+    # commit the changes
+    git add .
+    git commit -m "Syncing with LDES data"
+    git push origin main
+    git checkout -b restricted/ldes
+    git add .
+    git commit -m "Creating restricted/ldes branch"
+    git push origin restricted/ldes
+    git checkout main
+    echo "Making branches"
+    python -u ../../src/make_branches.py
 fi
