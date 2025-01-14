506f59972e626d9aec6476072730935203fd9a6d
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-01-14 15:55:40 +0100

    Remove obsolete LDES sync logic from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,28 +56,3 @@ bash setup.sh
 # everything in src folder and recursively under src folder
 # package-lock.json
 # actions.yml
-
-cd ..
-
-rsync --recursive --progress -avzhq --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
-cd ./github/workspace
-
-# commit the changes
-git add .
-git commit -m "Syncing with LDES data"
-git push origin main
-
-ls -al
-if [ "$BRANCHES" = *"restricted/ldes"* ]; then
-    # this means that the restricted/ldes branch exists
-    # so sync must be run 
-    echo "LDES sync branch exists"
-else
-    git checkout -b restricted/ldes
-    git add .
-    git commit -m "Creating restricted/ldes branch"
-    git push origin restricted/ldes
-    git checkout main
-    echo "Making branches"
-    python -u ../../src/make_branches.py
-fi
